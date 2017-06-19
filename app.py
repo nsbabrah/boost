620d57f893d5a7720095932dbf3fca6313a2fddd
akhullar <arjitkhullar@gmail.com>
2017-06-19 11:25:00 -0400

    update eight

--- app.py
+++ app.py
@@ -1,56 +1,58 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template, request
 import requests
-import jsonify
-from flask.ext.bcrypt import Bcrypt
-# from init import botstart
-from flask_login import LoginManager,login_user
+# import jsonify
+# from flask.ext.bcrypt import Bcrypt
+# # from init import botstart
+# from flask_login import LoginManager,login_user
 ## from controllers import *
-login_manager = LoginManager()
-app = Flask(__name__,static_folder='static', static_url_path='/static')
-login_manager.init_app(app)
-# Bcrypt Config
-from config import *
-from models import *
-app.config['SQLALCHEMY_DATABASE_URI'] = DB
+# login_manager = LoginManager()
+app = Flask(__name__, static_folder='static', static_url_path='/static')
+# login_manager.init_app(app)
+# # Bcrypt Config
+# from config import *
+# from models import *
+# app.config['SQLALCHEMY_DATABASE_URI'] = DB
 
 # from controllers import *
 # DB instance init
-db = SQLAlchemy(app)
+# db = SQLAlchemy(app)
 # bcrypt = Bcrypt(app)
-@login_manager.user_loader
-def load_user(id):
-    '''User Loader for flask-login
-    :params
-     user_id -> email
-    '''
-    user = User.query.get(id)
-    if user:
-        return user
-    else:
-        return None
-def commit(obj):
-	if type(obj) == list:
-		for i in obj:
-			db.session.add(i)
-		db.session.commit()
-	else:
-		db.session.add(obj)
-		db.session.commit()
+# @login_manager.user_loader
+# def load_user(id):
+#     '''User Loader for flask-login
+#     :params
+#      user_id -> email
+#     '''
+#     user = User.query.get(id)
+#     if user:
+#         return user
+#     else:
+#         return None
+# def commit(obj):
+# 	if type(obj) == list:
+# 		for i in obj:
+# 			db.session.add(i)
+# 		db.session.commit()
+# 	else:
+# 		db.session.add(obj)
+# 		db.session.commit()
+
 
 @app.route('/')
 def index():
-	#
-	# page = requests.get("http://google.com")
-	# page.status_code
-	# soup = BeautifulSoup(page.content, 'html.parser')
-	# soup.find_all(id="lga")
+    #
+    # page = requests.get("http://google.com")
+    # page.status_code
+    # soup = BeautifulSoup(page.content, 'html.parser')
+    # soup.find_all(id="lga")
 
-	return render_template('public/index.html')
-@app.route('/signin',methods=['POST','GET'])
+    return render_template('public/index.html')
+
+
+@app.route('/signin', methods=['POST', 'GET'])
 def index1():
     if request.method == 'GET':
         return render_template('public/signin.html')
-
 
     if request.method == 'POST':
 
@@ -64,36 +66,29 @@ def index1():
         if user:
 
             if user.is_password_correct(password):
-				login_user(user)
-				user.authenticated = True
-				db.session.add(user)
-				db.session.commit()
-				return render_template('admin_boostlikes/index.html')
-
-
-
-
-
-
-
+                login_user(user)
+                user.authenticated = True
+                db.session.add(user)
+                db.session.commit()
+                return render_template('admin_boostlikes/index.html')
 
 
 @app.route('/admin')
 def admin():
-	 if request.method == 'GET':
-		 return render_template('admin_boostlikes/index.html')
-	#admin
-	#admin
+    if request.method == 'GET':
+        return render_template('admin_boostlikes/index.html')
+        # admin
+        # admin
 
-@app.route('/signup', methods=['POST','GET'])
+
+@app.route('/signup', methods=['POST', 'GET'])
 def index2():
     if request.method == 'GET':
         return render_template('public/signup.html')
 
-
-	# json_data = requests.json
+        # json_data = requests.json
     # user = User(
-	#     name=json_data['name'],
+        #     name=json_data['name'],
     #     email=json_data['email'],
     #     password=json_data['password']
     # )
@@ -105,15 +100,14 @@ def index2():
     #     status = 'this user is already registered'
     # db.session.close()
     # return jsonify({'result': status})
-	#
-
+        #
 
     if request.method == 'POST':
 
         params = request.form
         username = params['username']
         password = params['password']
-        email    = params['email']
+        email = params['email']
         user = 'nav'
         user = User.query.filter_by(email=email)
         user = user.first()
@@ -121,52 +115,61 @@ def index2():
         if user:
 
             if user.is_password_correct(password):
-				login_user(user)
-				user.authenticated = True
-				db.session.add(user)
-				db.session.commit()
-				return redirect(url_for('admin_boostlikes/index.html'))
+                login_user(user)
+                user.authenticated = True
+                db.session.add(user)
+                db.session.commit()
+                return redirect(url_for('admin_boostlikes/index.html'))
 
 
 @app.route('/trynow')
 def trynow():
-	 if request.method == 'GET':
-		 return render_template('public/trynow.html')
-	#
+    if request.method == 'GET':
+        return render_template('public/trynow.html')
+        #
+
+
 @app.route('/home')
 def indexhome():
-	 if request.method == 'GET':
-		 return render_template('adminm/home.html')
-	#
+    if request.method == 'GET':
+        return render_template('adminm/home.html')
+        #
+
+
 @app.route('/about')
 def indexabout():
-	 if request.method == 'GET':
-		 return render_template('public/about.html')
+    if request.method == 'GET':
+        return render_template('public/about.html')
+
 
 @app.route('/Boost')
 def index3():
-	 if request.method == 'GET':
-		 return render_template('admin_boostlikes/Boost.html')
+    if request.method == 'GET':
+        return render_template('admin_boostlikes/Boost.html')
+
+
 @app.route('/Listlike')
 def index4():
-	 if request.method == 'GET':
-		 return render_template('admin_boostlikes/Listlike.html')
-	#
+    if request.method == 'GET':
+        return render_template('admin_boostlikes/Listlike.html')
+        #
+
 
 @app.route('/dashboard')
 def index5():
-	 if request.method == 'GET':
-		 return render_template('admin_boostlikes/index.html')
-	#
-@app.route('/AutoRound',methods=['POST','GET'])
-def index6():
-	 if request.method == 'GET':
-		 return render_template('admin_boostlikes/autoround.html')
-	 if request.method == 'POST':
-		#  engine.index6()
-		 return render_template('admin_boostlikes/autoround.html')
+    if request.method == 'GET':
+        return render_template('admin_boostlikes/index.html')
+        #
 
+
+@app.route('/AutoRound', methods=['POST', 'GET'])
+def index6():
+    if request.method == 'GET':
+        return render_template('admin_boostlikes/autoround.html')
+    if request.method == 'POST':
+           #  engine.index6()
+        return render_template('admin_boostlikes/autoround.html')
 
 
 if __name__ == '__main__':
-	app.run(host ='0.0.0.0',port=8002,debug=True,threaded=True)
+    app.run(host='localhost', port=8002, debug=True, threaded=True)
