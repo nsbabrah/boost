ee23e2cdc566d4ea16fbab42b587ab26b9a4a309
nsbabrah <navjotbabrah27@gmail.com>
2017-06-24 01:49:00 -0400

    13update

--- app.py
+++ app.py
@@ -34,45 +34,18 @@ payment = paypalrestsdk.Payment({
 "payment_method": "paypal" },
 "redirect_urls": {
 "return_url": "http://192.168.2.56:3000/dashboard",
-"cancel_url": "http://192.168.2.56:3000/" },
+"cancel_url": "http://192.168.2.56:3000/AutoRound" },
 
 "transactions": [ {
 "amount": {
-"total": "0.01",
+"total": "1.00",
 "currency": "CAD" },
 "description": "creating a payment" } ] })
-# payment = paypalrestsdk.Payment(payment)
 
-# api=paypalrestsdk.configure({
-#   'mode': 'sandbox',
-#   'client_id': 'AYIV-LTwqnn52pR-g-OHDhEp36DD3aw5PiKP866R3MVePcNg0NTSRgAUAbiUrZsyaJHv5o_L9fSOF3aA',
-#   'client_secret': 'gmLaDAgbQXYnSLh7vw5Hfg9Fo4vBSY-WwKAroE3fp2sEbP'
-# })
-
-# payment = paypalrestsdk.Payment({
-# "intent": "sale",
-# "payer": {
-# "payment_method": "paypal" },
-# "redirect_urls": {
-# "return_url": "http://127.0.0.1:8023/AutoRound",
-# "cancel_url": "http://127.0.0.1:8023/AutoRound" },
-#
-# "transactions": [ {
-# "amount": {
-# "total": "12",
-# "currency": "USD" },
-# "description": "creating a payment" } ] } )
-#
-#
-# payment.create()
-
-# verify='../../etc/ssl/certs/cacert.org.pem'
-# response = requests.get('https://lists.cacert.org', verify=verify)
-# Bcrypt Config
 from config import *
-from models import *
+# from models import *
 app.config['SQLALCHEMY_DATABASE_URI'] = DB
-app.config['SECRET_KEY'] = '769876tr8629r9yog^%&^*)&*^%&()'
+app.config['SECRET_KEY'] = '769876tr8629r9yog^%&^*13*^&)&*^%&()'
  # the payment transaction description."}]})
 
 login_manager.login_view = '/signin'
@@ -103,11 +76,7 @@ def commit(obj):
 
 @app.route('/')
 def index():
-    #
-    # page = requests.get("http://google.com")
-    # page.status_code
-    # soup = BeautifulSoup(page.content, 'html.parser')
-    # soup.find_all(id="lga")
+
 
     return render_template('public/index.html')
 
@@ -126,15 +95,7 @@ def index1():
         user = 'nav'
         user = User.query.filter_by(username=username)
         user = user.first()
-                # password = User.query.filter_by(password=password)
-        # # user = user.first()
-        # registered_user = User.query.filter_by(username=username,password=password).first()
-        # if registered_user is None:
-        #     flash('Username or Password is invalid' , 'error')
-        # return redirect(url_for('login'))
-        # login_user(registered_user)
-        # flash('Logged in successfully')
-        # return redirect(request.args.get('next') or url_for('index'))
+
         if 'remember' in params:
             remember = True
 
@@ -222,14 +183,7 @@ def index4():
 @app.route('/dashboard',methods=['GET'])
 def index5():
     if request.method == 'GET':
-        # payer_id = request.args.get('payerId')
-        # # payer_id = request.args.get('payerId')
-        # payment_id = request.args.get('paymentId')
-        # # token = request.args.get('token')
-        # # state=request.args.get('state')
-        # payment_token = request.args.get('token', '')
-        # billing_agreement_response = BillingAgreement.execute(str(request.args.get('payerId')))
-        # return redirect(url_for('/AutoRound'))
+
         payment_id = request.args.get('paymentId', None)
         payer_id = request.args.get('PayerID', None)
         payment = paypalrestsdk.Payment.find(payment_id)
@@ -250,19 +204,6 @@ def index5():
         else:
             return render_template('admin_boostlikes/index.html')
 
-            # db.session.commit()
-
-        # print payment_id
-        # print payer_id
-        # payment = paypalrestsdk.Payment.find(payment_id)
-        #
-        # if payment.execute({"payer_id": 'DUFRQ8GWYMJXC'}):
-        #   print("Payment execute successfully")
-        #   return render_template('admin_boostlikes/index.html')
-        #   #
-        # else:
-        #   print(payment.error) # Error Hash
-        #   return render_template('admin_boostlikes/autoround.html')
 
 
 
@@ -271,16 +212,8 @@ def index5():
 def index6():
 
     if request.method == 'GET':
-        #    payment = paypalrestsdk.Payment.find("PAY-57363176S1057143SKE2HO3A")
-           #
-        #     # Get List of Payments
-        #    payment_history = paypalrestsdk.Payment.all({"count": 10})
-        #    payment_history.payments
-
 
            return render_template('admin_boostlikes/autoround.html')
-            # Error Hash
-                        #  response.headers.add('Access-Control-Allow-Origin', '*')
 
 
 
@@ -292,7 +225,7 @@ def index6():
            for link in payment.links:
             if link.method == "REDIRECT":
                 redirect_url = str(link.href)
-                # response.headers.add('Access-Control-Allow-Origin', '*')
+
                 print('Redirect for approval: {}'.format(redirect_url))
                 url=('{}'.format(redirect_url))
                 # return re('Redirect for approval: {}'.format(redirect_url))direct(redirect_urls)
@@ -301,18 +234,6 @@ def index6():
                 # token = request.args.get('token')
                 return redirect(url)
 
-
-                # pending_payment = PayPalPayment.query.filter_by(token=token).filter_by(state='created').first_or_404()
-                #
-                # try:
-                #     payment = paypalrestsdk.Payment.find(pending_payment.payment_id)
-                # except paypalrestsdk.exceptions.ResourceNotFound as ex:
-                #     print('Paypal resource not found: {}'.format(ex))
-                #     abort(404)
-                #     if payment.execute({"payer_id": payer_id}):
-                #         pending_payment.state = payment.state
-                #         pending_payment.updated_at = datetime.strptime(payment.update_time, "%Y-%m-%dT%H:%M:%SZ")
-                #         db.session.commit()
 
 
 
@@ -336,65 +257,6 @@ def index6():
 
 
 
-
-
-
-    #     return redirect(url_for('/AutoRound'))
-    # else:
-    #     return redirect(url_for('/'))
-        # payment = paypalrestsdk.Payment({
-        #           "intent": "sale",
-        #           "payer": {
-        #           "payment_method": "paypal" },
-        #           "redirect_urls": {
-        #           "return_url": "https://127.0.0.1:8023/AutoRound/",
-        #           "cancel_url": "https://yoursite.com/canceled/" }, },api=api )
-        #
-        #
-        #
-        # # payment = paypalrestsdk.Payment(payment)
-        # if payment.create():
-        #     print("Payment[%s] created successfully" % (payment.id))
-        #     for link in payment.links:
-        #         if link.method == "REDIRECT":
-        #             redirect_url = str(link.href)
-        #             print('Redirect for approval: {}'.format(redirect_url))
-        #             return redirect(redirect_urls)
-
-# @app.route('/AutoRound', methods=['POST', 'GET'])
-# def indexendpoint():
-#
-#     if request.method == 'GET':
-#         payment = paypalrestsdk.Payment.find("Payment-ID")
-#         payment.execute({"payer_id": "Payer-ID "})
-#         return render_template('admin_boostlikes/index.html')
-#
-
-@app.route('/execute/', methods=['GET'])
-def payment_paypal_execute():
-    payment_id = request.GET.get('paymentId', None) or request.POST.get('paymentId', None)
-    payer_id = request.GET.get('PayerID', None) or request.POST.get('PayerID', None)
-    payment = paypalrestsdk.Payment.find(payment_id)
-    pending_payment = PayPalPayment.query.filter_by(token=token).filter_by(state='created').first_or_404()
-
-    try:
-        payment = paypalrestsdk.Payment.find(pending_payment.payment_id)
-    except paypalrestsdk.exceptions.ResourceNotFound as ex:
-        print('Paypal resource not found: {}'.format(ex))
-        abort(404)
-
-    if payment.execute({"payer_id": payer_id}):
-        pending_payment.state = payment.state
-        pending_payment.updated_at = datetime.strptime(payment.update_time, "%Y-%m-%dT%H:%M:%SZ")
-        db.session.commit()
-        return render_template('payment/success.html', payment_id=payment.id, state=payment.state)
-
-    return render_template('payment/error.html', payment_error=payment.error, step='Finallizing payment')
-    # Redirect the user to given approval url
-
-
-           #  engine.index6()
-        # return render_template('admin_boostlikes/autoround.html')
 
 
 if __name__ == '__main__':
