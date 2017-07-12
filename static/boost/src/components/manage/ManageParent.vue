9b376f7fd960b61346eff6e25b6d5935d781baba
Arjit Khullar <arjitkhullar@gmail.com>
2017-07-11 23:06:27 -0400

    fixed manage page formatting

--- static/boost/src/components/manage/ManageParent.vue
+++ static/boost/src/components/manage/ManageParent.vue
@@ -1,12 +1,14 @@
 <template>
   <main>
-    <v-container fluid>
+    <v-container fluid class="mt-3">
       <v-layout row wrap>
-        <v-flex xs12 sm12 lg12>
-          <v-select v-bind:items="change" v-model="selected_to_change" single-line auto required class="input-group--focused" prepend-icon="edit"></v-select>
+        <v-flex xs12 sm6 lg6>
+          <v-select v-bind:items="change" v-model="selected_to_change"required class="input-group--focused" prepend-icon="edit"></v-select>
+        </v-flex>
+         <v-flex xs12 sm6 lg6>
           <v-text-field name="input-1" :label="'Change '+selected_to_change" v-model="input" required></v-text-field>
         </v-flex>
-        <v-flex xs12 sm12 lg12>
+        <v-flex offset-xs4 offset-sm5 offset-md5>
           <v-btn primary light @click.native="save">Save</v-btn>
         </v-flex>
       </v-layout>
